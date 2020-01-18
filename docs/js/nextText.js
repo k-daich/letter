1e511c207b7f0660a97db265ac36b7d6dc9af726
daich <fst-qst.w-it-h@docomo.ne.jp>
2020-01-18 02:20:28 +0000

    20200118 MOD create to basic dispLikeTypeWriter

--- docs/js/nextText.js
+++ docs/js/nextText.js
@@ -14,36 +14,41 @@ function nextText() {
 /**
  * 以降、ファンクション
  */
+var dispCount = 0; // 現在表示されている文字数
+const dispSpeed = 100; // 1文字を表示する際のミリ秒
+/**
+ * アニメーション
+ */
+var animate = {
+
+    /**
+     * タイプライターのように一文字ずつ表示させる
+     */
+    dispLikeTypeWriter: function(element, sentence) {
+        dispCount++;
+        loggingObj('this', this);
+
+        if (dispCount > sentence.length) {
+            logging('LoopEnd');
+            dispCount = 0;
+            return;
+        }
+        logging('count : sentence', dispCount + ':' + sentence.substring(0, dispCount));
+        var type = sentence.substring(0, dispCount);
+
+        // テキストフィールドにデータを渡す処理
+        element.innerHTML = type;
+
+        setTimeout(animate.dispLikeTypeWriter, dispSpeed, element, sentence);
+    }
+}
+
 // マウスダウンした時に発火
 function mdown(event) {
     logging('mdown', 'start');
     loggingObj('subtitlesEle', this.subtitlesEle);
-    dispLikeTypeWriter(this.subtitlesEle, sentences[currentTextIndex++]);
+    animate.dispLikeTypeWriter(this.subtitlesEle, sentences[currentTextIndex++]);
 }
-
-
-
-/**
- * タイプライターのように一文字ずつ表示させる
- */
-var dispCount = 0; // 現在表示されている文字数
-const dispSpeed = 300; // 1文字を表示する際のミリ秒
-function dispLikeTypeWriter(element, sentence) {
-    dispCount++;
-    logging('count : sentence', dispCount + ':' + sentence);
-
-    if (dispCount > sentence.length) {
-        logging('end');
-        dispCount = 0;
-        return;
-    }
-    var type = sentence.substring(0, dispCount);
-    // テキストフィールドにデータを渡す処理
-    element.innerHTML = type;
-
-    var rep = setTimeout(dispLikeTypeWriter(element, sentence), dispSpeed);
-}
-
 
 /**
  * 以降、Util
--- docs/js/sentence/20200116.js
+++ docs/js/sentence/20200116.js
@@ -1,41 +1,27 @@
 var sentences = [
-"You try to run me through",
-"Hold on, think again",
-"Don't you know what's you're starting",
-"But you sure ain't got a clue",
-"",
-"How bad, this will go",
-"Don't you know my art, art of war",
-"",
-"",
-"And as you look to the horizon, not a cloud",
-"But then stormy weather's caught you cold",
-"Seems like it crept up out of nowhere all around you",
-"It's not quite what you foretold",
-"",
+"You try to run me through<br>\
+Hold on, think again<br>\
+Don't you know what's you're starting<br>\
+But you sure ain't got a clue",
+"How bad, this will go<br>\
+Don't you know my art, art of war",
+"And as you look to the horizon, not a cloud<br>\
+But then stormy weather's caught you cold<br>\
+Seems like it crept up out of nowhere all around you<br>\
+It's not quite what you foretold",
 "You'll never see it coming",
 "You'll see that my mind is too fast for eyes",
-"",
 "You're done in by the time it's hit you, your last surprise ",
-"",
-"",
 "You think you got your game planned out to a T  ",
 "Yet I'm two steps ahead yeah  ",
 "So you step into my way  ",
 "Stand down, it's a trap  ",
 "One more step and you're dead, year you're dead",
-"",
-"",
-"",
 "Why just a picosecond ago clear blue skies",
 "But now lightning's struck your last resolve",
 "It's not an accident that no one hears your cries",
 "As your last strength seems to dissolve",
-"",
-"",
-"",
 "You'll never see it coming",
 "You'll see that my mind is too fast for eyes",
 "You're done in by the time It's hit you, your last surprise",
-"",
-""];
+];
