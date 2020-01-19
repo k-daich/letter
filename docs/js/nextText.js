61ff667cbbf1a95eff03bc24b6ee8e3a71aef003
daich <fst-qst.w-it-h@docomo.ne.jp>
2020-01-19 01:06:12 +0000

    20200119 MOD resolve to duplicate run of disp like TypeWriter

--- docs/js/nextText.js
+++ docs/js/nextText.js
@@ -14,20 +14,29 @@ function nextText() {
 /**
  * 以降、ファンクション
  */
-const dispSpeed = 90; // 1文字を表示する際のミリ秒
+const dispSpeed = 50; // 1文字を表示する際のミリ秒
 /**
  * アニメーション
  */
 var animate = {
+    // 処理中フラグ
+    isRunning: false,
+    // 二重起動フラグ
+    isDuplicate: false,
     /*
      * アニメーションの実行手続き
      */
-    execute: function(element, sentence) {
+    run: function(element, sentence) {
+        // 処理中フラグを立てる
+        animate.isRunning = true;
         animate.nbspSet(element, sentence.split(/\n/));
         logging('replStr', sentence.replace(/\n/g, ''));
         animate.dispLikeTypeWriter(element, 0, Array.from(sentence.replace(/\n/g, '')));
     },
 
+    /*
+     * 表示する文字分だけのnbspを設定する
+     */
     nbspSet: function(element, text_array) {
         var init_html = '';
         for (index in text_array) {
@@ -40,18 +49,32 @@ var animate = {
     },
 
     /**
-     * タイプライターのように一文字ずつ表示させる
+     * タイプライターのようにnbspを一文字ずつ置換して文字を表示させていく
      */
     dispLikeTypeWriter: function(element, index, replcStr) {
-        if (replcStr.length == index) return;
+        // 二重起動した場合 OR 全ての表示が完了した場合
+        if (animate.isDuplicate || replcStr.length == index) {
+            // 処理中フラグを落とす
+            animate.isRunning = false;
+            return;
+        }
         // "全角一文字"⇔"nbsp2つ"を置換
         if (isZenkaku(replcStr[index])) {
             element.innerHTML = element.innerHTML.replace('&nbsp;&nbsp;', replcStr[index]);
-        // "半角一文字"⇔"nbsp1つ"を置換
+            // "半角一文字"⇔"nbsp1つ"を置換
         } else {
             element.innerHTML = element.innerHTML.replace('&nbsp;', replcStr[index]);
         }
         setTimeout(animate.dispLikeTypeWriter, dispSpeed, element, ++index, replcStr);
+    },
+
+    /**
+     * 即時に文字を全表示する
+     */
+    allDisp: function(element, sentence) {
+        element.innerHTML = sentence.replace(/\n/g, '<br>');
+        // 二重起動フラグを落とす
+        animate.isDuplicate = false;
     }
 }
 
@@ -66,7 +89,18 @@ function repalaceAt(str, index, char) {
 function mdown(event) {
     logging('mdown', 'start');
     loggingObj('subtitlesEle', this.subtitlesEle);
-    animate.execute(this.subtitlesEle, sentences[currentDispTextIndex++]);
+    // アニメーションが実行中の場合
+    if (animate.isRunning) {
+        // 二重起動フラグを立てる
+        animate.isDuplicate = true;
+        // 全ての文字を即時表示させる
+        // ※アニメーションの処理中であることを考慮し、ループ時間+αミリ秒だけ待機してから実行する
+        setTimeout(animate.allDisp, dispSpeed + 100, this.subtitlesEle, sentences[currentDispTextIndex-1]);
+    }
+    // アニメーションが実行中でない場合
+    else {
+        animate.run(this.subtitlesEle, sentences[currentDispTextIndex++]);
+    }
 }
 
 /**
--- docs/js/sentence/20200116.js
+++ docs/js/sentence/20200116.js
@@ -1,45 +1,49 @@
 var sentences = [
-"You try to run me through\n\
+    "テスト用文言。\n\
+改行もOK。\n\
+1バイトと2バイト文字が混在しようが問題無し。",
+
+    "You try to run me through\n\
 Hold on, think again\n\
 Don't you know what's you're starting\n\
 But you sure ain't got a clue",
 
-"How bad, this will go\n\
+    "How bad, this will go\n\
 Don't you know my art, art of war",
 
-"And as you look to the horizon, not a cloud\n\
+    "And as you look to the horizon, not a cloud\n\
 But then stormy weather's caught you cold\n\
 Seems like it crept up out of nowhere all around you\n\
 It's not quite what you foretold",
 
-"You'll never see it coming",
+    "You'll never see it coming",
 
-"You'll see that my mind is too fast for eyes",
+    "You'll see that my mind is too fast for eyes",
 
-"You're done in by the time it's hit you, your last surprise ",
+    "You're done in by the time it's hit you, your last surprise ",
 
-"You think you got your game planned out to a T  ",
+    "You think you got your game planned out to a T",
 
-"Yet I'm two steps ahead yeah  ",
+    "Yet I'm two steps ahead yeah",
 
-"So you step into my way  ",
+    "So you step into my way",
 
-"Stand down, it's a trap  ",
+    "Stand down, it's a trap",
 
-"One more step and you're dead, year you're dead",
+    "One more step and you're dead, year you're dead",
 
-"Why just a picosecond ago clear blue skies",
+    "Why just a picosecond ago clear blue skies",
 
-"But now lightning's struck your last resolve",
+    "But now lightning's struck your last resolve",
 
-"It's not an accident that no one hears your cries",
+    "It's not an accident that no one hears your cries",
 
-"As your last strength seems to dissolve",
+    "As your last strength seems to dissolve",
 
-"You'll never see it coming",
+    "You'll never see it coming",
 
-"You'll see that my mind is too fast for eyes",
+    "You'll see that my mind is too fast for eyes",
 
-"You're done in by the time It's hit you, your last surprise",
+    "You're done in by the time It's hit you, your last surprise",
 
 ];
