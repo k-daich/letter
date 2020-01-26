a29d76fb83c58968aefdee9b8afdc077a8704336
daich <fst-qst.w-it-h@docomo.ne.jp>
2020-01-26 09:07:29 +0000

    20200126 ADD maven java project

---
+++ app/letter/src/test/java/jp/daich/letter/letter/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.daich.letter.letter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
